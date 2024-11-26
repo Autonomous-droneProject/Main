84cabf561f918e490a7b5d7c96a0d16d1b6e3471
JylerT <146783468+JylerTackson@users.noreply.github.com>
2024-11-26 18:00:30 -0500

    Update yoloHandler.py

--- sharedModules/YOLO/yoloHandler.py
+++ sharedModules/YOLO/yoloHandler.py
@@ -1,21 +1,20 @@
 class YOLOModelHandler:
 
-    def __init__(self, model_path: str, confidence: float = 0.3):
+    def __init__(self, ):
         """
         Constructor for the YOLO model handler.
         """
 
 
     
-    def train_model(self, data_path: file_name, epochs: int = 100, img_size: int = 720):
+    def train_model(self, ):
         """
         Trains the YOLO model on a specified dataset.
-            data_path (str): Path to the dataset YAML file.
         """
 
 
     
-    def detect(self, video_path: str, save: bool = True, show: bool = True, classes: list = None):
+    def detect(self, ):
         """
         Runs object detection and returns results.
         """
