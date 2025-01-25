2f5f2e1de8740ae29daa96460f646681e2ab618c
Manuel <95004633+MEbran06@users.noreply.github.com>
2025-01-25 14:17:43 -0500

    Create kalmanTest.py

---
+++ sharedModules/YOLO/kalmanTest.py
@@ -0,0 +1,59 @@
+import cv2
+import time
+from ultralytics import YOLO
+from yolo_kalman import KalmanTracker, convert_bbox_to_z
+import numpy as np
+
+trt_model = YOLO('yolo11n.pt')
+
+# Open the video file
+video_path = 'PATH_TO_VIDEO'
+cap = cv2.VideoCapture(video_path)
+
+
+# We need to set resolutions. 
+# so, convert them from float to integer. 
+frame_width = int(cap.get(3)) 
+frame_height = int(cap.get(4)) 
+fps = cap.get(cv2.CAP_PROP_FPS)
+   
+size = (frame_width, frame_height) 
+
+# Define the codec and create a VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('stream.avi', fourcc, 20, size)
+
+#t1 = time.time()
+fc = 0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    fc += 1
+
+    # Run inference
+    results = trt_model.predict(frame)
+
+    # Process results (similar to your existing code)
+    for result in results:
+        bboxes = result.boxes.xywh.cpu().numpy().astype(int)
+        if fc == 1: predBBoxes = KalmanTracker()
+        tracks = predBBoxes.bboxes_to_tracks(bboxes)
+
+        for nbbox in predBBoxes.pred_tracks():
+            #if fc == 1: predBBoxes.kalman.x = np.append(convert_bbox_to_z(track[-1]), [0,0,0])
+            x1 = int(nbbox[0] - nbbox[2]/2)
+            y1 = int(nbbox[1] - nbbox[3]/2)
+            x2 = int(nbbox[0] + nbbox[2]/2)
+            y2 = int(nbbox[1] + nbbox[3]/2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    # Write the frame to the VideoWriter object
+    out.write(frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+out.release()
+cap.release()
+cv2.destroyAllWindows()
